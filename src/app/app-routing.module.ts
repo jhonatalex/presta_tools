2f87222f0afe02bc131fe30fe53aa5ff09532511
unknown <abjuntadevelop@gmail.com>
2023-05-16 14:26:35 -0430

    Primer commit Hialmar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ToolDetailComponent } from './components/tool-detail/tool-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'acceso',component:LoginComponent},
+  {path:'registro',component:RegisterComponent},
+  {path: 'detalle/:id',component: ToolDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/models/tool.model.ts
@@ -0,0 +1,10 @@
+export class Tool{
+    constructor(
+        public id:number,
+        public nombre:string,
+        public descripcion:string,
+        public valor:number,
+        public peso:number,
+        public imagen:string
+    ){}
+}
