52453d4f1d1239ae2e8f999f6ad74ac27318bc26
jhonatalex <jhonatanmejias@gmail.com>
2023-08-28 21:53:15 -0400

    deploy agosto 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,9 +4,9 @@ export const environment = {
   production: true,
   baseUrl: new Constants().getProdApiURL(),
   firebaseConfig : {
-    apiKey: import.meta.env.NG_APP_FIREBASE.apiKey ,
-    authDomain: import.meta.env.NG_APP_FIREBASE.authDomain,
-    projectId: import.meta.env.NG_APP_FIREBASE.proyecId,
+    apiKey: "AIzaSyChCasCEmUn6TzMTTPSUITxLL5uZ6TRxTI",
+    authDomain: "prestatools-35370.firebaseapp.com",
+    projectId: "prestatools-35370",
     storageBucket: "prestatools-35370.appspot.com",
     messagingSenderId: "218432434957",
     appId: "1:218432434957:web:4f0ae3ac846cb2debaf644",
