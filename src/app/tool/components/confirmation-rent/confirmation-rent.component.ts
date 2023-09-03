366f3508da762f6d8f0dcc15ab5d35acb6e1cd94
jhonatalex <jhonatanmejias@gmail.com>
2023-09-03 11:08:55 -0300

    publicacion domingo2

--- src/app/tool/components/confirmation-rent/confirmation-rent.component.ts
+++ src/app/tool/components/confirmation-rent/confirmation-rent.component.ts
@@ -148,7 +148,7 @@ this.route.params.subscribe(params =>{
 
 
     var amount:number = this.total;
-    var returnUrl ="http://localhost:4200/confirmar-transaccion"
+    var returnUrl = environment.production ? 'https://jhonatalex.github.io/presta_tools/confirmar-transaccion' : '"http://localhost:4200/confirmar-transaccion" '
 
 
     //VENTA
@@ -214,7 +214,7 @@ this.route.params.subscribe(params =>{
     form.submit();
   }
 
- /* 
+ /*
   onSubmitPay(formulario:NgForm) {
 
     const formElement = document.getElementById(
