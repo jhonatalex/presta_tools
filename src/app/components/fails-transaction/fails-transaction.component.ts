acc1950b36fef3be694edc4c3c1fa69b14a93909
Hialfer <abjuntadevelop@gmail.com>
2023-11-12 09:39:44 -0400

    ajustes responsive tool-detail

--- src/app/components/fails-transaction/fails-transaction.component.ts
+++ src/app/components/fails-transaction/fails-transaction.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PaymentServices } from 'src/app/payment/providers/payment.service';
+import { User } from 'src/app/register/models/user.model';
 
 @Component({
   selector: 'app-fails-transaction',
@@ -6,8 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fails-transaction.component.css']
 })
 export class FailsTransactionComponent implements OnInit {
+  apiResponse : any;
+  detalleVenta:any;
+  venta:any;
+  public tool: any;
+  public user: User;
 
-  constructor() { }
+  constructor(
+    private payServices: PaymentServices,
+  ) {this.user = new User;
+    
+    payServices.getData().subscribe({
+
+    next: data =>{
+    this.apiResponse = data;
+    this.venta = this.apiResponse.data;
+    this.detalleVenta = this.apiResponse.data.detalleVenta[0];
+    this.tool = this.apiResponse.data.detalleVenta[0].idToolNavigation;
+    this.user = this.apiResponse.data.idUserNavigation;
+
+
+    console.log(data);
+    console.log(this.user);
+
+    }
+  }) }
 
   ngOnInit(): void {
   }
